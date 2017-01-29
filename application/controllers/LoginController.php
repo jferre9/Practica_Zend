bcfcdf2d01c8e9c01287ac364d677af6deb982c6
Usuari <Usuari@P109>
2017-01-30 00:39:34 +0100

    a

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -10,7 +10,16 @@ class LoginController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $form = new Zend_Form();
+        $csv = new Zend_Form_Element_File('csv');
+        $csv->setLabel("Importar dades en format csv:")
+                ->addValidator('Count', false, 1)
+                ->addValidator('Extension', false, 'csv')
+                ->setRequired();
+        
+        $form->setAction('/login/importar');
+        $form->setMethod('post');
+        $form->addElement($csv);
     }
 
 
--- application/models/DbTable/Alumne.php
+++ application/models/DbTable/Alumne.php
@@ -1,10 +1,15 @@
 <?php
 
-class Application_Model_DbTable_Alumne extends Zend_Db_Table_Abstract
-{
+class Application_Model_DbTable_Alumne extends Zend_Db_Table_Abstract {
 
     protected $_name = 'alumne';
 
+    public function login($dni, $pass) {
+        $select = $this->select()->where('dni = ?', $dni)->where('pass = ?',$pass);
+    }
+    
+    public function insertar($data) {
+        $this->insert($data);
+    }
 
 }
-
